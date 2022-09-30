86677200385e237f7ef77b17ebe2bcc8fce3d0b8
Lyncolnlopes <112292071+Lyncolnlopes@users.noreply.github.com>
2022-09-30 00:36:18 -0300

    animated background

---
+++ background-java/java-exerc.js
@@ -0,0 +1,13 @@
+document.getElementById('verde').addEventListener('mouseenter', function(){
+    document.querySelector('body').setAttribute('class','bnt-verde');
+})
+document.getElementById('vermelho').addEventListener('mouseenter', function(){
+    document.querySelector('body').setAttribute('class','bnt-vermelho');
+})
+document.getElementById('amarelo').addEventListener('mouseenter', function(){
+    document.querySelector('body').setAttribute('class','bnt-amarelo');
+})
+
+document.getElementById('roxo').addEventListener('mouseenter', function(){
+document.querySelector('body').setAttribute('class','bnt-roxo')
+})
